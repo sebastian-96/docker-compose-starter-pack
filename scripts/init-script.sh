bfe017bff4ddd48d228df9798497e029c1a338c4
sebastian-96 <sebastian.gral96@gmail.com>
2023-12-15 11:47:30 +0100

    Updated repo with samples

---
+++ scripts/init-script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Initialization Script for Docker Environment
+
+echo "Initializing Docker environment..."
+
+# Set script execution rights
+# Replace 'script.sh' with the name of your script
+# chmod +x script.sh
+
+# Set environment variables
+# Uncomment and set the environment variables as needed
+# export VAR_NAME="value"
+
+# Example of setting up execution rights for another script
+# chmod +x your-other-script.sh
+
+# Add any other environment setup tasks here
+# This could include downloading dependencies, setting up databases,
+# configuring network settings, etc.
+
+# Display a message to indicate completion
+echo "Docker environment is now set up and ready to use!"
+
+# You can also execute other scripts or commands at the end of the initialization
+# ./your-other-script.sh
+# docker-compose up -d
